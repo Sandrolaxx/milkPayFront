d5a146bf10a4922806e5126a912445d084cb623d
Sandrolaxx <sandro--ramos@hotmail.com>
2022-04-21 22:32:43 -0300

    Finish login screen

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const withReactSvg = require('next-react-svg')
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
+  
 }
 
 module.exports = nextConfig
+
+module.exports = withReactSvg({
+    include: path.resolve(__dirname, "src/assets/icons"),
+    webpack(config, options) {
+      return config
+    },
+})
