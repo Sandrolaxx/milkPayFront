989ec589efdb7e67fd863230fef08c38488fea99
Sandrolaxx <sandro--ramos@hotmail.com>
2022-08-01 21:10:39 -0300

    Fix decimal values

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -196,23 +196,9 @@ export function getDefaultPageSize() {
     return defaultPageSize;
 }
 
-export function getTotalInterest(dailyInterest: number, dueDate: Date): number {
+export function getTotalInterest(dailyInterest: number, dueDate: Date): string {
     var timeDiff = Math.abs(dueDate.getTime() - new Date().getTime());
     var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-    return Math.floor(diffDays * dailyInterest);
-}
-
-export function formatDecimalValue(num: number, places: number) {
-    if (!("" + num).includes("e")) {
-        return +(Math.round(num.toString() + "e+" + places.toString()) + "e-" + places);
-    } else {
-        let arr = ("" + num).split("e");
-        let sig = ""
-        if (+arr[1] + places > 0) {
-            sig = "+";
-        }
-
-        return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + places)) + "e-" + places);
-    }
+    return (diffDays * dailyInterest).toFixed(2);
 }
