963b5a60445c39de791eb86ecc189d1d176ccee9
Sandrolaxx <sandro--ramos@hotmail.com>
2022-11-12 20:55:53 -0300

    create list title screen

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,11 @@
 import { ButtonProps } from "../utils/types";
 
-export default function Button({ text, handleFunction, stylized }: ButtonProps) {
+export default function Button({ text, handleFunction, dafaultStyle, customStile }: ButtonProps) {
     return (
-        <button onClick={e => handleFunction(e)} className={`${stylized ? ` w-full py-2 px-4 text-center  
+        <button onClick={e => handleFunction(e)} className={`${dafaultStyle ? `w-full py-2 px-4 text-center  
             bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 
             text-base text-white font-semibold shadow-md transition ease-in duration-200  
-            focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg` : false}`}>
+            focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg` : customStile}`}>
             {text}
         </button>
     );
